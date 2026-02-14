322ba666dbc62d662657ef088830de02d8e85166
Oleh Blazhko <96749215+docholypancake@users.noreply.github.com>
2026-02-14 22:47:52 +0200

    Fix string quotation style in worker.js for consistency

--- js/worker.js
+++ js/worker.js
@@ -3,15 +3,15 @@ let timerInterval = null;
 self.onmessage = function(e) {
     const command = e.data;
 
-    if (command === 'start') {
+    if (command === "start") {
         //if there's already an interval running, clear it before starting a new one
         if (timerInterval) clearInterval(timerInterval);
         
         // Start a new interval that sends a 'tick' message every second
         timerInterval = setInterval(() => {
-            self.postMessage('tick');
+            self.postMessage("tick");
         }, 1000);
-    } else if (command === 'stop') {
+    } else if (command === "stop") {
         if (timerInterval) clearInterval(timerInterval);
         timerInterval = null;
     }
