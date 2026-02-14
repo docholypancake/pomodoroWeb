9684370822cfd6cf2916ff2592cca1650b6404c3
Oleh Blazhko <96749215+docholypancake@users.noreply.github.com>
2026-02-14 22:07:42 +0200

    Refactor comments in worker.js for clarity and consistency

--- js/worker.js
+++ js/worker.js
@@ -4,10 +4,10 @@ self.onmessage = function(e) {
     const command = e.data;
 
     if (command === 'start') {
-        // Якщо вже запущено, спочатку очищаємо, щоб не дублювати
+        //if there's already an interval running, clear it before starting a new one
         if (timerInterval) clearInterval(timerInterval);
         
-        // Запускаємо інтервал
+        // Start a new interval that sends a 'tick' message every second
         timerInterval = setInterval(() => {
             self.postMessage('tick');
         }, 1000);
