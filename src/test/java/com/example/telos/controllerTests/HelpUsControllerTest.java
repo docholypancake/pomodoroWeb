ddd8886c7dac73b1808f6e2a5800c2c6737788a0
auiechko <auechko@gmail.com>
2026-03-11 10:07:11 +0200

    Created mock test

---
+++ src/test/java/com/example/telos/controllerTests/HelpUsControllerTest.java
@@ -0,0 +1,25 @@
+package com.example.telos.controllerTests;
+
+
+import com.example.telos.controller.HelpUsController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(HelpUsController.class)
+public class HelpUsControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void shouldReturnAboutPage() throws Exception{
+        mockMvc.perform(get("/about"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("about"));
+    }
+}
